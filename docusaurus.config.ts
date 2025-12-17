04d25ff4ed75a18a8542378c2aaa8d4bb4fd5dab
Aaron <4709780+2manyvcos@users.noreply.github.com>
2025-12-17 10:26:35 +0000

    fix pipeline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "reeveci", // Usually your GitHub org/user name.
   projectName: "reeveci.github.io", // Usually your repo name.
+  deploymentBranch: "pages",
 
   trailingSlash: false,
   onBrokenLinks: "throw",
